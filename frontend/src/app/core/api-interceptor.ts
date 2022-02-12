ddf0384d9f7fd9c369d3d3baeb8a480d20e171a2
Kevin Tam <kevin@kevintamcs.com>
2022-02-12 01:12:26 -0500

    Add API interceptor to auto-append API base domain

---
+++ frontend/src/app/core/api-interceptor.ts
@@ -0,0 +1,14 @@
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { environment } from 'src/environments/environment';
+
+
+@Injectable()
+export class ApiInterceptor implements HttpInterceptor {
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const apiReq = req.clone({ url: environment.apiUrl + req.url });
+    return next.handle(apiReq);
+  }
+}
